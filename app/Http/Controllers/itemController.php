ecbed0605ce675936fccc4dbb3a53ae64c6da09c
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-03-25 09:25:11 +0530

    frame

---
+++ app/Http/Controllers/itemController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    //
+    public function read()
+    {
+        $uid = auth()->user()->id;
+        $items = Item::where('uid', $uid)->get();
+        return view('index', ['items' => $items]);
+    }
+    public function create()
+    {
+        return view('create');
+    }
+    public function createConfirm(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+        $uid = auth()->user()->id;
+        $data['uid'] = $uid;
+        $newitem = item::create($data);
+        return redirect(route('reading'))->with('success', 'Item Created SuccessFully');
+    }
+    public function update(item $item)
+    {
+        return view('update', ['item' => $item]);
+    }
+    public function updateConfirm(Request $request, item $item)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'description' => 'required'
+        ]);
+        $item->update($data);
+        return redirect(route('reading'))->with('success', 'Item Updated SuccessFully');
+    }
+    public function delete(item $item)
+    {
+        $item->delete();
+        return redirect(route('reading'))->with('success', 'Item Deleted SuccessFully');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\AuthManager;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [AuthManager::class, 'login'])->name('login');
+Route::post('/', [AuthManager::class, 'loginPost'])->name('login.post');
+
+Route::get('/register', [AuthManager::class, 'register'])->name('register');
+Route::post('/register', [AuthManager::class, 'registerPost'])->name('register.post');
+Route::get('/logout', [AuthManager::class, 'logout'])->name('logout');
+
+Route::get('/welcome', [ItemController::class, 'read'])->name('reading');
+Route::get('/create', [ItemController::class, 'create'])->name('creating');
+Route::post("/create", [ItemController::class, 'createConfirm'])->name('create.confirm');
+Route::get('/update{item}', [ItemController::class, 'update'])->name('updating');
+Route::post("/update{item}", [ItemController::class, 'updateConfirm'])->name('update.confirm');
+Route::get("/{item}", [ItemController::class, 'delete'])->name('deleting');
