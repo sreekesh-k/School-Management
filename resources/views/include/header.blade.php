04d707bfb263ebf6404edff4102cda654a4a2b3a
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-03-25 10:59:54 +0530

    hide register

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
+        <a class="navbar-brand" href="/forstudents">For Teachers</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -10,13 +10,6 @@
                 @auth
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('logout') }}">Logout</a>
-                    </li>
-                @else
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">Register</a>
                     </li>
                 @endauth
             </ul>
