7e9288117f791844b9c543117a25a1b7fbd1445f
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-03-25 11:46:29 +0530

    teacherspage

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -11,6 +11,13 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('logout') }}">Logout</a>
                     </li>
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">Register</a>
+                    </li>
                 @endauth
             </ul>
             <span clas="navbar-text"> @auth
